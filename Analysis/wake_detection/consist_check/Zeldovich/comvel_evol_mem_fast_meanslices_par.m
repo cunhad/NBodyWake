a0a6124c247ef0a2ed8d695d36585214e23fbc82
cunhad <disrael.camargo@gmail.com>
2019-11-19 11:26:10 +0100

    added new analysis, 'clara', to the 3d wavelet

--- Analysis/wake_detection/consist_check/Zeldovich/comvel_evol_mem_fast_meanslices_par.m
+++ Analysis/wake_detection/consist_check/Zeldovich/comvel_evol_mem_fast_meanslices_par.m
@@ -340,7 +340,7 @@ for rds=1:length(redshift_list)
     half_mp_mod(rds)=(mode(abs(mean_vel_per_slice_)));
     
 %     indx_quart=[length(Pos_Bin_Centers)/8:3*length(Pos_Bin_Centers)/8,5*length(Pos_Bin_Centers)/8:7*length(Pos_Bin_Centers)/8];
-    indx_quart=[length(Pos_Bin_Centers)/8:2*length(Pos_Bin_Centers)/8,6*length(Pos_Bin_Centers)/8:7*length(Pos_Bin_Centers)/8];
+    indx_quart=[length(Pos_Bin_Centers)/8:2*length(Pos_Bin_Centers)/8,6 *length(Pos_Bin_Centers)/8:7*length(Pos_Bin_Centers)/8];
     Pos_Bin_Centers_quart=Pos_Bin_Centers(indx_quart);
     mean_vel_per_slice_quart=mean_vel_per_slice(indx_quart);
     fig=figure('Visible', 'off');
