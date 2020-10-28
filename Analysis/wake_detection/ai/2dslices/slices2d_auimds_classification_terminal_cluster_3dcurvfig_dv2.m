1e8979b928824b6262dfdc11d5251118b520e2e8
cunhad <disrael.camargo@gmail.com>
2020-10-28 10:22:46 +0100

    ai color dataset 3d curve and non-parallel 2

--- Analysis/wake_detection/ai/2dslices/slices2d_auimds_classification_terminal_cluster_3dcurvfig_dv2.m
+++ Analysis/wake_detection/ai/2dslices/slices2d_auimds_classification_terminal_cluster_3dcurvfig_dv2.m
@@ -21,17 +21,17 @@ slices=32;
 N_angles=12*NSIDE*NSIDE/2;
 N_angles_t=12*NSIDE*NSIDE;
 
-% specs_path_list_nowake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_nowakem'
+specs_path_list_nowake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_nowakem'
 % specs_path_list_nowake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpx_2d_NSIDE4_ai_fig/4Mpc_2048c_1024p_zi63_nowakem'
-specs_path_list_nowake='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_nowakem'
+% specs_path_list_nowake='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_nowakem'
 sample_list_nowake=dir(strcat(specs_path_list_nowake,'/sample*'));
 sample_list_nowake={sample_list_nowake.name};
 % sample_list_nowake=sort_nat(sample_list_nowake)
 
-% specs_path_list_wake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_wake_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
+specs_path_list_wake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_wake_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
 % specs_path_list_wake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
 % specs_path_list_wake='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_hpx_2d_NSIDE4_wake_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
-specs_path_list_wake='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_wake_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
+% specs_path_list_wake='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_wake_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
 % specs_path_list_wake='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_hpx_2d_NSIDE4_wake_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
 sample_list_wake=dir(strcat(specs_path_list_wake,'/sample*'));
 sample_list_wake={sample_list_wake.name};
@@ -40,8 +40,8 @@ sample_list_wake=strcat(sample_list_wake,'/half_lin_cutoff_half_tot_pert_nvpw_v0
 % sample_list_wake=sort_nat(sample_list_wake)
 
 
-% specs_path_list_wake_nonparal='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
-specs_path_list_wake_nonparal='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
+specs_path_list_wake_nonparal='/home/cunhad/projects/rrg-rhb/cunhad/simulations/cubep3m/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
+% specs_path_list_wake_nonparal='/home/asus/Dropbox/extras/storage/graham/ht/data_cps32_512_hpxNSIDE4_2dclaral1lr1na1024_and_3dparclaral1lr1_visual_fig/4Mpc_2048c_1024p_zi63_wakeGmu4t10m8zi10m'
 sample_list_wake_nonparal=dir(strcat(specs_path_list_wake_nonparal,'/sample*'));
 sample_list_wake_nonparal={sample_list_wake_nonparal.name};
 sample_list_wake_nonparal=strcat(sample_list_wake_nonparal,'/half_lin_cutoff_half_tot_pert_nvpw_v0p6');
